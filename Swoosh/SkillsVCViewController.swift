88e13647b05c79a0794883dd38bbf4beb831e6b0
Harshal Solanki <harshal.solanki46@gmail.com>
2017-08-26 21:08:24 +0530

    Segues done

---
+++ Swoosh/SkillsVCViewController.swift
@@ -0,0 +1,41 @@
+//
+//  SkillsVCViewController.swift
+//  Swoosh
+//
+//  Created by Harshal Solanki on 26/08/17.
+//  Copyright © 2017 Harshal Solanki. All rights reserved.
+//
+
+import UIKit
+
+class SkillsVCViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func unwindFromCategoryVC(unwindSegues : UIStoryboardSegue){
+        
+    }
+    
+    
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- Swoosh/ViewController.swift
+++ Swoosh/ViewController.swift
@@ -24,13 +24,19 @@ class ViewController: UIViewController {
         
         bgImg.frame = view.frame;
  */
+        
+    
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    
+    @IBAction func unwindFromSkillVC(unwindSegues : UIStoryboardSegue){
+        
     }
-
+    
+    
+    
+   
 
 }
 
