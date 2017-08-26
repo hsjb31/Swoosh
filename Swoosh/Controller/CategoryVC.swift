8f67644c0f96e53c21fd4052969ebc43de9f0da5
Harshal Solanki <harshal.solanki46@gmail.com>
2017-08-26 21:35:48 +0530

    refactoringInXcode

---
+++ Swoosh/Controller/CategoryVC.swift
@@ -0,0 +1,20 @@
+//
+//  CategoryVC.swift
+//  Swoosh
+//
+//  Created by Harshal Solanki on 26/08/17.
+//  Copyright © 2017 Harshal Solanki. All rights reserved.
+//
+
+import UIKit
+
+class CategoryVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    
+}
---
+++ Swoosh/Controller/LeagueVC.swift
@@ -0,0 +1,24 @@
+//
+//  LeagueVC.swift
+//  Swoosh
+//
+//  Created by Harshal Solanki on 26/08/17.
+//  Copyright © 2017 Harshal Solanki. All rights reserved.
+//
+
+import UIKit
+
+class LeagueVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func unwindCategoryVC(unwindSegues : UIStoryboardSegue){
+        
+        
+    }
+
+}
